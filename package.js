cc924278a2a7fb57252fa7fade271e43b109b5a3
roy <roygiladix@gmail.com>
2018-04-22 11:25:12 +0300

    fixed styled packages loading

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'meteoreact:accounts',
   summary: 'Simple and intuative accounts view layer with react',
-  version: '1.1.5',
+  version: '1.1.6',
   documentation: 'README.md',
   git: 'https://github.com/royGil/accounts-react'
 })
