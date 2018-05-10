cd6d413b5c70bd0de77fc27ddad264cd47718572
roy <roygiladix@gmail.com>
2018-05-10 23:45:33 +0300

    removed deepmerge dependency

--- __tests__/client.test.js
+++ __tests__/client.test.js
@@ -1,6 +1,6 @@
 import './setup'
 
 Meteor.startup(() => {
-  /* Load tests only after meteor's startup to ensure the package has loaded entirley */
+  /* Load tests only after meteor's startup to ensure the package has been entirley loaded */
   require('./_client/forms')
 })
